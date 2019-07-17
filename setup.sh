5369b9358bcd16127a754fef3cb10b551878aee3
nitr8 <nitr8@nfye.com>
2019-07-17 16:01:28 +0200

    few fixes

--- setup.sh
+++ setup.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
-if [[ -f /tmp/CrushFTP9_PC.zip ]] ; then
-    echo "Unzipping CrushFTP..."
-    unzip -o -q /tmp/CrushFTP9_PC.zip -d /var/opt/
-    rm -f /tmp/CrushFTP9_PC.zip
+if [ -f /var/opt/CrushFTP9_PC/installed ] ; then
+	echo "CrushFTP installed"
+else
+  echo "Initializing CrushFTP"
+  unzip -o -q /tmp/CrushFTP9_PC.zip -d /var/opt/
+  rm -f /tmp/CrushFTP9_PC.zip
+  touch /var/opt/CrushFTP9_PC/installed
+  echo "Removing crap"
+  rm -Rf /var/opt/CrushFTP9_PC/CrushFTP*.exe /var/opt/CrushFTP9_PC/Install.txt
+  echo " " > /var/opt/CrushFTP9_PC/WebInterface/custom.css
 fi
 
 [ -z ${CRUSH_ADMIN_USER} ] && CRUSH_ADMIN_USER=crushadmin
 [ -z ${CRUSH_ADMIN_PASSWORD} ] && CRUSH_ADMIN_PASSWORD=crushadmin
 
-if [[ ! -d /var/opt/CrushFTP9_PC/users/MainUsers/${CRUSH_ADMIN_USER} ]] || [[ -f /var/opt/CrushFTP9_PC/admin_user_set ]] ; then
-    echo "Creating default admin..."
+if [[ -f /var/opt/CrushFTP9_PC/admin_set ]] ; then
+    echo "Admin is already set"
+else
+    echo "Creating admin user: ${CRUSH_ADMIN_USER} with password: ${CRUSH_ADMIN_PASSWORD}"
     cd /var/opt/CrushFTP9_PC && java -jar /var/opt/CrushFTP9_PC/CrushFTP.jar -a "${CRUSH_ADMIN_USER}" "${CRUSH_ADMIN_PASSWORD}"
-    touch /var/opt/CrushFTP9_PC/admin_user_set
+    touch /var/opt/CrushFTP9_PC/admin_set
 fi
 
 /var/opt/run-crushftp.sh start
